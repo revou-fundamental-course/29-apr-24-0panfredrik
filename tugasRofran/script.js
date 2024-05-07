baf6ad5534658869c4398226ac03d43180d56c15
0panfredrik <168445505+0panfredrik@users.noreply.github.com>
2024-05-07 20:58:08 +0800

    Add files via upload

---
+++ tugasRofran/script.js
@@ -0,0 +1,16 @@
+// function replaceName(){
+// let nama = prompt("halo", "");
+// document.getElementById("nama").innerHTML = nama;
+// }
+
+// document.getElementById('tombol').addEventListener("click");
+
+// var slideshow = 1 ();
+
+
+// let nameInput = document.getElementById('name-input')
+// document.getElementById('Submit').addEventListener("click", function(){prompt()})
+// function replaceName(){
+// let name = prompt ("Siapakah Nama Anda", "");
+// document.getElementById("Name").innerHTML = name}
+// document.getElementById('tombol').addEventListener("click",function(){replaceName();})
